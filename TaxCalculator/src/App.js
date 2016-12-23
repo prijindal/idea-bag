5f81142cda803cfb10c3507acf409a6eb1464388
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-12-23 13:27:19 +0530

    Added Basic Eslint

--- TaxCalculator/src/App.js
+++ TaxCalculator/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 class App extends Component {
+  state = {
+    amount: 0,
+  }
+
+  onChangeText = (value) => {
+    this.setState({
+      amount: value,
+    });
+  }
+
   render() {
+    const { amount } = this.state;
     return (
       <View>
-        <Text>Hello</Text>
+        <TextInput
+            keyboardType="numeric"
+            onChangeText={this.onChangeText}
+        />
+        <Text>{amount}</Text>
       </View>
     );
   }
